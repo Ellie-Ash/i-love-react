1f52e83a773e45fae12216f8e4f0fbe895a9ebaf
Ellie Ash <ellieash4000@gmail.com>
2019-07-01 09:07:16 -0500

    my-app

--- src/components/animals/AnimalCard.js
+++ src/components/animals/AnimalCard.js
@@ -1,23 +1,23 @@
-// import React, { Component } from "react"
-// import { Link } from "react-router-dom"
-// import DogIcon from "./DogIcon.png"
-// import "./Animals.css"
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import DogIcon from "./DogIcon.png"
+import "./Animals.css"
 
-// export default class AnimalCard extends Component {
-//     render() {
-//         return (
-//             <div key={this.props.animal.id} className="card">
-//                 <div className="card-body">
-//                     <h5 className="card-title">
-//                         <img src={DogIcon} alt="dog" className="icon--dog" />
-//                         {this.props.animal.name}
-//                         <Link className="nav-link" to={`/animals/${this.props.animal.id}`}>Details</Link>
-//                         <a href="google.com"
-//                             onClick={() => this.props.deleteAnimal(this.props.animal.id)}
-//                             className="card-link">Delete</a>
-//                     </h5>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
+export default class AnimalCard extends Component {
+    render() {
+        return (
+            <div key={this.props.animal.id} className="card">
+                <div className="card-body">
+                    <h5 className="card-title">
+                        <img src={DogIcon} alt="dog" className="icon--dog" />
+                        {this.props.animal.name}
+                        <Link className="nav-link" to={`/animals/${this.props.animal.id}`}>Details</Link>
+                        <a href="google.com"
+                            onClick={() => this.props.deleteAnimal(this.props.animal.id)}
+                            className="card-link">Delete</a>
+                    </h5>
+                </div>
+            </div>
+        )
+    }
+}
